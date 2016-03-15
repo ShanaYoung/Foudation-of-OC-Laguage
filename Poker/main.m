45a6230540f686540090c3d0b07d72e762fc8f9a
ShanaYoung <315729267@qq.com>
2016-03-15 01:53:38 -0700

    Update main.m

--- Poker/main.m
+++ Poker/main.m
@@ -6,33 +6,20 @@
 //  Copyright © 2016年 Shana. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-#import "Poker.h"
-#import "Hands.h"
-#import "Human.h"
-
 int main(int argc, const char * argv[]) {
-    Poker *p1=[[Poker alloc] init];
-    [p1 setNum:'K'];
-    [p1 setColor:@"红桃"];
+    Poker * p1 = [[Poker alloc] initWithHua:@"❤️" andNum:@"K"];
+    [p1 show];
+    Hand * h1 = [[Hand alloc] initWithPoker:p1];
+    [h1 show];
     
-    Poker *p2=[[Poker alloc] init];
-    [p2 setNum:'A'];
-    [p2 setColor:@"黑桃"];
+    Hand * h2 = [[Hand alloc] init];
+    [h2 setPoker:[[Poker alloc] initWithHua:@"♠️" andNum:@"A"]];
     
-    NSLog(@"左手%@%c  右手%@%c",[p1 color],[p1 num],[p2 color],[p2 num]);
+    Human * per = [[Human alloc] initWithName:@"小明" andRight:h2 andLeft:h1];
     
-    Hands *hand=[[Hands alloc] init];
-    [hand setLeft:p1];
-    
-    [hand setRight:p2];
-    
-    Human *hum=[[Human alloc] init];
-    [hum setLeft:[hand right]];
-    [hum setRight:[hand left]];
-    
-    NSLog(@"左手%@%c  右手%@%c",[[hum left] color],[[hum left] num],[[hum right] color],[[hum right] num]);
-    
+    [per show];
+    [per change];
+    [per show];
     
     return 0;
 }
